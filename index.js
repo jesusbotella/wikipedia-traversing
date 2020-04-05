3b6f3c4e19a7da0f3e1b52ed60d80403baf4e8a2
Jesús Botella <jesus.botella@gmail.com>
2020-04-05 15:13:02 +0200

    Add ESLint

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const axios = require('axios');
 const debug = require('debug')('wikipedia-traversing');
+
 debug('Starting');
 
 // Create Graph
-const Graph = require('graphlib').Graph;
+const { Graph } = require('graphlib');
